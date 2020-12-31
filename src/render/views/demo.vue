c5d625716d885cc09412d046c6d71580432704f1
xiebiao <1404027855@qq.com>
2020-12-31 14:07:09 +0800

    init

---
+++ src/render/views/demo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    const message = ref<string>('my mesddsage');
+    return { message };
+  },
+});
+</script>
